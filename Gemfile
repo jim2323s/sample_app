4bbef7e90922242626d5efca40c12f4782f0041c
Jim Steensen <jim2323s@aol.com>
2011-05-16 20:52:00 -0500

    partial validations

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'rails', '3.0.4'
   group :development do
     gem "sqlite3-ruby", :require => "sqlite3"
 		gem 'rspec-rails', '2.0.1'
+		gem 'annotate-models', '1.0.4'
   end
   
   group :test do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe User do
+
+  before(:each) do
+		@attr = { :name => "Example User", :email => "user@example.com" }
+	end
+	
+	it "should create a new instance given valid attributes" do
+		User.create!(@attr)
+	end
+	
+	it "should require a name" do
+		no_name_user = User.new(@attr.merge(:name => ""))
+		no_name_user.should_not be_valid
+	end
+	
+	it "should require an email address" do
+		no_email_user = User.new(@attr.merge(:email => ""))
+		no_email_user.should_not be_valid
+	end
+	
+	it "should reject names that are too long" do
+		longName = "a" * 51
+		longNameUser = User.new(@attr.merge(:name => ""))
+		longNameUser.should_not be_valid
+	end
+	
+	#pending "add some examples to (or delete) #{__FILE__}"
+end
