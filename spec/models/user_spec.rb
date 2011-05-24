7dafb799fe682fd705aa55cf0dbfbc8fe556e45b
Jim Steensen <jim2323s@aol.com>
2011-05-23 19:21:16 -0500

    finished chap 6

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,5 +26,36 @@ describe User do
 		longNameUser.should_not be_valid
 	end
 	
+	it "should accept valid email addresses" do
+		addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+		addresses.each do |address|
+			valid_email_user = User.new(@attr.merge(:email => address))
+			valid_email_user.should be_valid
+		end
+	end
+	
+	it "should reject invalid email addresses" do
+		addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+		addresses.each do |address|
+			invalid_email_user = User.new(@attr.merge(:email => address))
+			invalid_email_user.should_not be_valid
+		end
+	end
+	
+	it "should reject duplicate email addresses" do
+		# Put a user with given email address into the database
+		User.create!(@attr)
+		user_with_duplicate_email = User.new(@attr)
+		user_with_duplicate_email.should_not be_valid
+	end
+	
+	it "should reject duplicate email addresses insensitive to case"
+		upcased_email = @attr[:email].upcase
+		User.create!(@attr.merge(:email => upcased_email))
+		user_with_duplicate_email = User.new(@attr)
+		user_with_duplicate_email.should_not be_valid
+	end
+
+	
 	#pending "add some examples to (or delete) #{__FILE__}"
 end
